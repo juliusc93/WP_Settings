b7e3c6990a79bae1e5a18a719ee99bb89367c82b
Julio C. Visbal Ruiz <jvisbalc@uninorte.edu.co>
2015-03-09 11:19:51 -0500

    Settings - Windows Phone

---
+++ App_Settings/EditPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.IO.IsolatedStorage;
+
+namespace App_Settings
+{
+    public partial class EditPage : PhoneApplicationPage
+    {
+        public IsolatedStorageSettings appSettings;
+        public EditPage()
+        {
+            InitializeComponent();
+            appSettings = IsolatedStorageSettings.ApplicationSettings;
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            try
+            {
+                txtName.Text = appSettings["name"].ToString();
+                txtAge.Text = appSettings["age"].ToString();
+                rbtnMale.IsChecked = (bool)appSettings["male"];
+                rbtnFemale.IsChecked = (bool)appSettings["female"];
+            }
+            catch (Exception)
+            {
+                appSettings.Add("name", txtName.Text);
+                appSettings.Add("age", txtAge.Text);
+                appSettings.Add("male", rbtnMale.IsChecked);
+                appSettings.Add("female", rbtnFemale.IsChecked);
+                appSettings.Save();
+            }
+        }
+
+        private void btnSave_Tap(object sender, GestureEventArgs e)
+        {
+            appSettings["name"] = txtName.Text;
+            appSettings["age"] = txtAge.Text;
+            appSettings["male"] = rbtnMale.IsChecked;
+            appSettings["female"] = rbtnFemale.IsChecked;
+            appSettings.Save();
+
+            //Go back to the Main Page
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+        
+    }
+}
---
+++ App_Settings/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.IO.IsolatedStorage;
+
+namespace App_Settings
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public IsolatedStorageSettings appSettings;
+        public MainPage()
+        {
+            InitializeComponent();
+            appSettings = IsolatedStorageSettings.ApplicationSettings;
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            try
+            {
+                if (String.IsNullOrWhiteSpace(appSettings["name"].ToString())) txtName.Text = "Not assigned";
+                else txtName.Text = appSettings["name"].ToString();
+                if (String.IsNullOrWhiteSpace(appSettings["age"].ToString())) txtAge.Text = "Not assigned";
+                else txtAge.Text = appSettings["age"].ToString();
+                if ((bool)appSettings["male"]) txtGender.Text = "Male";
+                else if ((bool)appSettings["female"]) txtGender.Text = "Female";
+                else txtGender.Text = "Not assigned";
+            }
+            catch (Exception)
+            {
+                appSettings.Add("name", "");
+                appSettings.Add("age", "");
+                appSettings.Add("male", false);
+                appSettings.Add("female", false);
+                appSettings.Save();
+            }
+        }
+
+        private void btnEdit_Tap(object sender, GestureEventArgs e)
+        {
+            //Go to the Edit Page
+            NavigationService.Navigate(new Uri("/EditPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
